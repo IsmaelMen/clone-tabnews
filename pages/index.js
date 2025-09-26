284444a22a1d842fcea8ac0dc2a3a362eed9db6f
Ismaelmendes <ismaelmendes935@gmail.com>
2025-09-26 03:24:14 +0000

    add scripts `lint:check` and `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Era pra ser uma frase de carinho, mas eu clonei o repositorio e fiz outro </h1>
+  return (
+    <h1>
+      Era pra ser uma frase de carinho, mas eu clonei o repositorio e fiz
+      outro
+    </h1>
+  );
 }
 
 export default Home;
